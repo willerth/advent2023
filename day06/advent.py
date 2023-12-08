e19475905ce402e2aeb88029687f912f6bcfcb45
Adam Willerth <a.willerth1@gmail.com>
2023-12-08 13:50:56 -0600

    I ONLY WIN

--- day06/advent.py
+++ day06/advent.py
@@ -1,3 +1,7 @@
+import time
+
+startTime = time.time()
+
 #INPUT HANDLING
 inputFile = open('day06/input.txt')
 inputTxt = inputFile.read().split(':')[1:]
@@ -5,10 +9,21 @@ inputFile.close()
 
 times = inputTxt[0].strip().split()[:-1]
 distances = inputTxt[1].split()
-for i in range(len(times)):
-    times[i] = int(times[i])
-    distances[i] = int(distances[i])
 
+
+timeAllowed = int(("".join(times)))
+distance = int(("".join(distances)))
+possibilities = 0
+for j in range(timeAllowed):
+    holdTime = j
+    moveTime = timeAllowed - j
+    distTraveled = holdTime * moveTime
+    if distTraveled > distance:
+        possibilities += 1
+
+print(possibilities)
+print(f'Code executed in {time.time() - startTime} seconds')
+exit()
 
 #MAIN SECTION
 result = 1
