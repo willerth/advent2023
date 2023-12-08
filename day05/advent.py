433f00a1904482dd8b90ba8b677c6e57ea2a3572
Adam Willerth <a.willerth1@gmail.com>
2023-12-08 12:39:42 -0600

    day 5 pass 1 - works perfectly on the example input but is slow

---
+++ day05/advent.py
@@ -0,0 +1,47 @@
+#INPUT HANDLING
+inputFile = open('day05/input.txt')
+inputSections = inputFile.read().split(':')[1:]
+
+seeds = inputSections[0].split()[:-2]
+for i in range(len(seeds)):
+    seeds[i] = int(seeds[i])
+
+mapRangeArrays = []
+maps = []
+results = []
+
+for i in range(1, len(inputSections)):
+    mapRangeArrays.append([])
+    section = inputSections[i]
+    sectionLines = section.strip().split('\n')
+
+    for j in range(len(sectionLines)):
+        if(sectionLines[j] == ''): break
+        mapRangeArrays[i - 1].append(sectionLines[j])
+
+    #break
+
+
+for i in range(len(mapRangeArrays)):
+    mapRangeArray = mapRangeArrays[i]
+    maps.append({})
+    for mapRange in mapRangeArray:
+        numberForm = mapRange.split()
+        mapStart = int(numberForm[1])
+        mapEnd = int(numberForm[0])
+        mapRange = int(numberForm[2])
+        for j in range(mapRange):
+            print(f'we are on {i}, {j}')
+            maps[i][mapStart + j] = mapEnd + j
+
+#MAIN SECTION
+for seed in seeds:
+    for map in maps:
+        seed = map[seed] if seed in map.keys() else seed
+    
+    results.append(seed)
+
+results.sort()
+print(results)
+        
+    
