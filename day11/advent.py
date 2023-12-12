cbab46e7b2f03df42bf833c9d7f70fcb15cc8416
Adam Willerth <a.willerth1@gmail.com>
2023-12-12 16:23:33 -0600

    day11pt2

--- day11/advent.py
+++ day11/advent.py
@@ -5,12 +5,21 @@ inputFile = open('day11/input.txt')
 inputTxt = inputFile.readlines()
 inputFile.close()
 
-universe = transposed(expand(transposed(expand(inputTxt))))
+universe = []
+
+for i in range(len(inputTxt)):
+    universe.append(inputTxt[i].strip())
+
+
+EMPTY_ROWS = findEmptyRows(universe)
+EMPTY_COLUMNS = findEmptyRows(transposed(universe))
 
 numColumns = len(universe[0])
 
 galaxies = []
 sum = 0
+
+
 
 for i in range(len(universe)):
     for j in range(numColumns):
@@ -22,10 +31,7 @@ for i in range(len(galaxies)):
     origin = galaxies[i]
     for j in range(i + 1, len(galaxies)):
         terminal = galaxies[j]
-        xDist = abs(origin[0] - terminal[0])
-        yDist = abs(origin[1] - terminal[1])
-        sum += xDist + yDist
+        sum += calculateDistance(origin, terminal, EMPTY_ROWS, EMPTY_COLUMNS, 1_000_000)
 
-    #break
 
 print(sum)
--- day11/util.py
+++ day11/util.py
@@ -10,12 +10,42 @@ def transposed(list):
 
     return result
 
-def expand(universe):
+
+def findEmptyRows(list):
+    result = []
+    for i in range(len(list)):
+        if'#' not in list[i]:
+            result.append(i)
+    return result
+
+
+def calculateDistance(galaxyA, galaxyB, emptyRows, emptyColumns, expansionCoefficient=2):
+    ax = galaxyA[1]
+    bx = galaxyB[1]
+    ay = galaxyA[0]
+    by = galaxyB[0]
+
+    distance = 0
+
+    xIncrement = 1 if ax < bx else -1
+    yIncrement = 1 if ay < by else -1
+
+
+    for y in range(ay, by, yIncrement):
+        distance += expansionCoefficient if y in emptyRows else 1
+
+    for x in range(ax, bx, xIncrement):
+        distance += expansionCoefficient if x in emptyColumns else 1
+    return distance
+
+
+def expand(universe, multiplier=2):
     result = []
     for line in universe:
         result.append(line.strip())
         if '#' not in line:
-            result.append(line.strip())
+            for i in range(1, multiplier):
+                result.append(line.strip())
     
     return result
 
