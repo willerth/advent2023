10deeaa3cb9f8c091a894c2676c75b8e5e960d5e
Adam Willerth <a.willerth1@gmail.com>
2023-12-18 09:44:56 -0600

    day15pt2

--- day15/advent.py
+++ day15/advent.py
@@ -5,9 +5,45 @@ inputFile = open('day15/input.txt')
 inputText = inputFile.read().split(',')
 inputFile.close()
 
+boxes = {}
+
+for value in inputText:
+    labelLength = value.index('-') if '-' in value else value.index('=')
+    label = value[:labelLength]
+    boxNo = hash(label)
+    if boxNo not in boxes.keys(): boxes[boxNo] = []
+    box = boxes[boxNo]
+
+    operation = value[labelLength]
+    if operation == '-':
+        for lens in box:
+            if lens[0] == label:
+                box.remove(lens)
+                #break
+
+    else:
+        focalLength = int(value[labelLength + 1])
+        labelAlreadyPresent = False
+        for lens in box:
+            if lens[0] == label:
+                lens[1] = focalLength
+                labelAlreadyPresent = True
+                #break
+        if not labelAlreadyPresent:
+            box.append([label, focalLength])
+
+
+
 
 sum = 0
-for value in inputText:
-    sum += hash(value)
+for boxNo in boxes.keys():
+    multiplier = boxNo + 1
+    box = boxes[boxNo]
+    position = 1
+    for lens in box:
+        focalLength = lens[1]
+        sum += multiplier * position * focalLength
+        position += 1
+
 
 print(sum)
